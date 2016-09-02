b60df0f1a0e4f4ab1543b527fc0522165e68329f
Alan Wills <alan.wills@junifersystems.com>
2016-09-02 14:41:09 +0100

    Pushing beginnings of OpenGL tutorials

---
+++ OpenGL/OpenGL/Main.cpp
@@ -0,0 +1,27 @@
+#include "Main.h"
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+
+Main::Main(void)
+{
+}
+
+
+Main::~Main(void)
+{
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	return 0;
+}
