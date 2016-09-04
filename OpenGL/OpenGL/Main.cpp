5807f9d0e39b2e94e2e6f964caf6aab8c750c547
AlanWills <alawills@googlemail.com>
2016-09-04 13:43:30 +0100

    Set up window

--- OpenGL/OpenGL/Main.cpp
+++ OpenGL/OpenGL/Main.cpp
@@ -1,27 +1,75 @@
 #include "Main.h"
 
-// GLEW
-#define GLEW_STATIC
-#include <GL/glew.h>
-// GLFW
-#include <GLFW/glfw3.h>
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
 
-Main::Main(void)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-}
+  // When a user presses the escape key, we set the WindowShouldClose property to true
+  // closing the application
 
-
-Main::~Main(void)
-{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+  {
+    glfwSetWindowShouldClose(window, GL_TRUE);
+  }
 }
 
 int main()
 {
+  // Set some GL constants
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+  // Create a GL window
+  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+  if (window == nullptr)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  // Tell the current thread it is associated with this window
+  glfwMakeContextCurrent(window);
+
+  // Initialize glew before we start calling any OpenGL functions
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK)
+  {
+    std::cout << "Failed to initialize GLEW" << std::endl;
+    return -1;
+  }
+
+  // Set the size of the rendering window
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+
+  // Set up the key callback for quitting the application
+  glfwSetKeyCallback(window, key_callback);
+
+  // Set the clear colour
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+  // Game loop to keep the window open
+  while (!glfwWindowShouldClose(window))
+  {
+    // Check and call events
+    glfwPollEvents();
+
+    // Render here
+
+    // Clear the buffer
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Swap the buffers
+    glfwSwapBuffers(window);
+  }
+
+  // Clean up resources
+  glfwTerminate();
 	return 0;
 }
--- OpenGL/OpenGL/Main.h
+++ OpenGL/OpenGL/Main.h
@@ -1,8 +1,17 @@
 #pragma once
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
 class Main
 {
-public:
-	Main(void);
-	~Main(void);
-};
 
+private:
+  void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+};
