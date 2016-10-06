3d75f085b144e24892680e6d3624ab11ac2fbfb6
AlanWills <alawills@googlemail.com>
2016-10-06 14:53:50 +0100

    Starting work on PoolAllocator implementation

--- Engine/Headers/Memory/PoolAllocator.h
+++ Engine/Headers/Memory/PoolAllocator.h
@@ -5,12 +5,58 @@
 namespace Engine
 {
 
+/// A memory allocator which creates a large pool of objects in a contiguous array
+/// Allocating from this consists of obtaining a pointer to an object in the pool not currently used
 template <typename T, size_t PoolSize>
 class DllExport PoolAllocator
 {
+  public:
+    PoolAllocator();
+    ~PoolAllocator();
+
+    /// \brief Obtain a pointer to a free object from this pool
+    /// The object will have already been constructed
+    /// This class still retains ownership of the object
+    T* allocate();
+
+    /// \brief Marks the object as free to be allocated again
+    void free(T* objectToFree);
+
+  private:
+    size_t m_head;
+    T m_pool[PoolSize];
 };
 
-};
+//------------------------------------------------------------------------------------------------
+template <typename T, size_t PoolSize>
+PoolAllocator<T, PoolSize>::PoolAllocator() :
+  m_head(0)
+{
+  // Construct all the objects
+  for (size_t i = 0; i < PoolSize; ++i)
+  {
+    m_pool[i] = T();
+  }
+}
 
-// Make this class templated and store a big array of objects and do degragmentation and all the stuff described in GEA
-// Call constructor of all pooled objects at start
+//------------------------------------------------------------------------------------------------
+template <typename T, size_t PoolSize>
+PoolAllocator<T, PoolSize>::~PoolAllocator()
+{
+}
+
+//------------------------------------------------------------------------------------------------
+template <typename T, size_t PoolSize>
+T* PoolAllocator<T, PoolSize>::allocate()
+{
+  return m_pool[m_head++];
+}
+
+//------------------------------------------------------------------------------------------------
+template <typename T, size_t PoolSize>
+void PoolAllocator<T, PoolSize>::free(T* objectToFree)
+{
+  ASSERT_FAIL();
+}
+
+};
---
+++ TestEngine/Source/Tests/Memory/TestPoolAllocator.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+
+#include "Memory/PoolAllocator.h"
+
+using namespace Engine;
+
+namespace TestEngine
+{
+  /*
+  In this class we use ints specifically and do not test other types
+  Should the need arise, extra types may need to be tested, but functionally I currently believe these tests provide enough coverage
+  */
+  TEST_CLASS(TestPoolAllocator)
+  {
+  public:
+
+    //------------------------------------------------------------------------------------------------
+    TEST_METHOD(Test_PoolAllocator_Constructor_NoErrors)
+    {
+      PoolAllocator<int, 10> intPool;
+      PoolAllocator<char, 10> charPool;
+      PoolAllocator<void*, 10> pointerPool;
+    }
+  };
+}
