286633c413e7c416ebd7c57169f24f0139e538dc
AlanWills <alawills@googlemail.com>
2016-10-09 13:09:05 +0100

    Refactored resource manager so it also uses custom allocators for efficiency.  Need to write unit tests now

--- Engine/Headers/Resources/ResourceManager.h
+++ Engine/Headers/Resources/ResourceManager.h
@@ -12,12 +12,15 @@
 #include "FileSystem/File.h"
 #include "FileSystem/Directory.h"
 #include "FileSystem/Path.h"
+#include "StringInterning/StringId.h"
+
+#include "Memory/PoolAllocator.h"
 
 using namespace Kernel;
 
 namespace Engine
 {
-#define DIRECTORY "C:/Users/Alan/Documents/Visual Studio 2015/Projects/OpenGL/OpenGL/OpenGL/"
+#define DIRECTORY "../OpenGL"
 #define TEXTURE_DIR "Assets"
 #define SHADER_DIR "Shaders"
 #define LEVEL_DIR "Levels"
@@ -25,8 +28,16 @@ namespace Engine
 #define FRAGMENT_SHADER_DIR "Fragment"
 #define GEOMETRY_SHADER_DIR "Geometry"
 
-typedef std::map<std::string, std::unique_ptr<Shader>>     ShaderMap;
-typedef std::map<std::string, std::unique_ptr<Texture2D>>  TextureMap;
+#define SHADER_POOL_SIZE 10
+#define TEXTURE_POOL_SIZE 100
+
+typedef std::map<StringId, Shader*>                     ShaderMap;
+typedef PoolAllocator<Shader, SHADER_POOL_SIZE>         ShaderPool;
+typedef std::vector<std::unique_ptr<Shader>>            ShaderPoolOverflow;
+
+typedef std::map<StringId, Texture2D*>                  TextureMap;
+typedef PoolAllocator<Texture2D, TEXTURE_POOL_SIZE>     TexturePool;
+typedef std::vector<std::unique_ptr<Texture2D>>         TexturePoolOverflow;
 
 // A static singleton ResourceManager class that hosts several
 // functions to load Textures and Shaders. Each loaded texture
@@ -36,20 +47,20 @@ typedef std::map<std::string, std::unique_ptr<Texture2D>>  TextureMap;
 class DllExport ResourceManager
 {
   public:
-    /// \brief Initialises resource specific data including glew and file paths
+    /// \brief Initialises resource specific data including glew and file paths and loads all resources that will last the lifetime of the game
     static void init();
 
     // Loads (and generates) a shader program from file loading vertex, fragment (and geometry) shader's source code. If gShaderFile is not nullptr, it also loads a geometry shader
-    static Shader* loadShader(const GLchar *vShaderFile, const GLchar *fShaderFile, const GLchar *gShaderFile, const std::string& name);
+    static Shader* loadShader(const GLchar *vShaderFile, const GLchar *fShaderFile, const GLchar *gShaderFile, StringId name);
 
     // Retrieves a stored shader
-    static Shader* getShader(const std::string& name);
+    static Shader* getShader(StringId name);
 
     // Loads (and generates) a texture from file
-    static Texture2D* loadTexture(const GLchar *file, GLboolean alpha, const std::string& name);
+    static Texture2D* loadTexture(const GLchar *file, GLboolean alpha, StringId name);
 
     // Retrieves a stored texture
-    static Texture2D* getTexture(const std::string& name);
+    static Texture2D* getTexture(StringId name);
 
   private:
     // Private constructor, that is we do not want any actual resource manager objects. Its members and functions should be publicly available (static).
@@ -64,6 +75,15 @@ class DllExport ResourceManager
     // Resource storage
     static ShaderMap   m_shaders;
     static TextureMap  m_textures;
+
+    // Pre-allocated memory for resources - we can overrun this, but it will be less efficient as memory will have to be dynamically allocated.
+    static ShaderPool  m_shaderPool;
+    static TexturePool m_texturePool;
+
+    // The overflow vectors we use to store pointers to dynamically created resources
+    // Ideally we want the X_POOL_SIZE to be large enough to never use these, but unexpected things may happen that we should account for
+    static ShaderPoolOverflow   m_shaderOverflow;
+    static TexturePoolOverflow  m_textureOverflow;
 };
 
 }
--- Engine/Source/Resources/ResourceManager.cpp
+++ Engine/Source/Resources/ResourceManager.cpp
@@ -7,8 +7,14 @@ namespace Engine
 {
   //------------------------------------------------------------------------------------------------
   // Instantiate static variables
-  TextureMap   ResourceManager::m_textures;
-  ShaderMap    ResourceManager::m_shaders;
+  ShaderMap             ResourceManager::m_shaders;
+  TextureMap            ResourceManager::m_textures;
+
+  ShaderPool            ResourceManager::m_shaderPool;
+  TexturePool           ResourceManager::m_texturePool;
+
+  ShaderPoolOverflow    ResourceManager::m_shaderOverflow;
+  TexturePoolOverflow   ResourceManager::m_textureOverflow;
 
 
   //------------------------------------------------------------------------------------------------
@@ -19,37 +25,67 @@ namespace Engine
   }
 
   //------------------------------------------------------------------------------------------------
-  Shader* ResourceManager::loadShader(const GLchar *vShaderFile, const GLchar *fShaderFile, const GLchar *gShaderFile, const std::string& name)
+  Shader* ResourceManager::loadShader(const GLchar *vShaderFile, const GLchar *fShaderFile, const GLchar *gShaderFile, StringId name)
   {
     Shader* shader = loadShaderFromFile(vShaderFile, fShaderFile, gShaderFile);
-    m_shaders[name] = std::unique_ptr<Shader>(shader);
+    m_shaders[name] = shader;
     return shader;
   }
 
   //------------------------------------------------------------------------------------------------
-  Shader* ResourceManager::getShader(const std::string& name)
+  Shader* ResourceManager::getShader(StringId name)
   {
-    return m_shaders[name].get();
+    if (m_shaders.find(name) == m_shaders.end())
+    {
+      ASSERT_FAIL_MSG("Shader file does not exist");
+
+      /// TODO: Return a default shader or something?
+      return nullptr;
+    }
+
+    return m_shaders[name];
   }
 
   //------------------------------------------------------------------------------------------------
-  Texture2D* ResourceManager::loadTexture(const GLchar *file, GLboolean alpha, const std::string& name)
+  Texture2D* ResourceManager::loadTexture(const GLchar *file, GLboolean alpha, StringId name)
   {
     Texture2D* texture = loadTextureFromFile(file, alpha);
-    m_textures[name] = std::unique_ptr<Texture2D>(texture);
+    m_textures[name] = texture;
     return texture;
   }
 
   //------------------------------------------------------------------------------------------------
-  Texture2D* ResourceManager::getTexture(const std::string& name)
+  Texture2D* ResourceManager::getTexture(StringId name)
   {
-    return m_textures[name].get();
+    if (m_textures.find(name) == m_textures.end())
+    {
+      ASSERT_FAIL_MSG("Texture does not exist");
+
+      /// TODO: Return a default texture or something?
+      return nullptr;
+    }
+
+    return m_textures[name];
   }
 
   //------------------------------------------------------------------------------------------------
   Shader* ResourceManager::loadShaderFromFile(const GLchar* vShaderFile, const GLchar* fShaderFile, const GLchar* gShaderFile)
   {
-    // 1. Retrieve the vertex/fragment/geometry source code from filePath
+    // Create shader object
+    Shader* shader;
+    if (m_shaderPool.canAllocate())
+    {
+      // If we have room left in the pool we just allocate a new entry
+      shader = m_shaderPool.allocate();
+    }
+    else
+    {
+      // If we have run out of room in our pool, we dynamically create a new shader and then store it in the overflow vector
+      shader = new Shader();
+      m_shaderOverflow.push_back(std::unique_ptr<Shader>(shader));
+    }
+
+    // Retrieve the vertex/fragment/geometry source code from filePath
     std::string vertexCode;
     std::string fragmentCode;
     std::string geometryCode;
@@ -83,8 +119,7 @@ namespace Engine
     const GLchar *fShaderCode = fragmentCode.c_str();
     const GLchar *gShaderCode = geometryCode.c_str();
 
-    // 2. Now create shader object from source code
-    Shader* shader = new Shader();
+    // Compile into the graphics card
     shader->compile(vShaderCode, fShaderCode, gShaderFile != nullptr ? gShaderCode : nullptr);
 
     return shader;
@@ -94,7 +129,19 @@ namespace Engine
   Texture2D* ResourceManager::loadTextureFromFile(const GLchar* file, GLboolean alpha)
   {
     // Create Texture object
-    Texture2D* texture = new Texture2D();
+    Texture2D* texture;
+    if (m_texturePool.canAllocate())
+    {
+      // If we have room left in the pool we just allocate a new entry
+      texture = m_texturePool.allocate();
+    }
+    else
+    {
+      // If we have run out of room in our pool, we dynamically create a new texture and then store it in the overflow vector
+      texture = new Texture2D();
+      m_textureOverflow.push_back(std::unique_ptr<Texture2D>(texture));
+    }
+
     if (alpha)
     {
       texture->setInternalFormat(GL_RGBA);
